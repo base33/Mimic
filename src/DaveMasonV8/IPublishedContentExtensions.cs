17475bd506fed10e280f47afe53ba7479cbc77de
Scott Rosseter <scott.rosseter@mentordigital.co.uk>
2020-02-25 11:36:30 +0000

    added v8 support

---
+++ src/DaveMasonV8/IPublishedContentExtensions.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using DaveMason.Context;
+using DaveMason.PropertyMapperAttributes;
+using Umbraco.Core.Models;
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Core.Persistence.Mappers;
+
+namespace DaveMason
+{
+    public static class IPublishedContentExtensions
+    {
+        public static T As<T>(this IPublishedContent content)
+        {
+            var type = typeof (T);
+
+            return (T)As(content, type);
+        }
+
+        public static List<T> As<T>(this IEnumerable<IPublishedContent> contents)
+        {
+            var type = typeof(T);
+
+            return contents.Select(content => (T) As(content, type)).ToList();
+        }
+
+        public static object As(this IPublishedContent content, Type type)
+        {
+            if (content == null)
+                return null;
+
+            var context = new MapperContext {Content = content};
+
+            var instance = Activator.CreateInstance(type);
+
+            foreach (var property in type.GetProperties(BindingFlags.Public | BindingFlags.Instance).Where(p => p.CanWrite))
+            {
+                context.Property = property;
+
+                var mapper = ResolveMapper(content, type, property);
+
+                if (mapper is Ignore)
+                    continue;
+
+                mapper.Context = context;
+
+                var value = mapper.ProcessValue();
+
+                property.SetValue(instance, value);
+            }
+
+            return instance;
+        }
+
+        private static PropertyMapperAttribute ResolveMapper(IPublishedContent content, Type type, PropertyInfo property)
+        {
+            //resolve by attribute
+            var attribute = property.GetCustomAttributes().FirstOrDefault(a => a is PropertyMapperAttribute);
+            if (attribute != null)
+            {
+                return (PropertyMapperAttribute)attribute;
+            }
+
+            return new SimpleType();
+        }
+    }
+}
---
+++ src/DaveMasonV8/PropertyMapperAttributes/SimpleType.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Umbraco.Core;
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Web;
+using Umbraco.Web.Composing;
+
+namespace DaveMason.PropertyMapperAttributes
+{
+    public class SimpleType : PropertyMapperAttribute
+    {
+        public override object ProcessValue()
+        {
+            var value = ResolveStictProperty();
+
+            return value ?? ResolveUmbracoProperty();
+        }
+
+        protected object ResolveStictProperty()
+        {
+            switch (Context.Property.Name)
+            {
+                case "Id":
+                    return Content.Id;
+                case "Name":
+                    return Content.Name;
+                case "Url":
+                    return Content.Url;
+            }
+
+            return null;
+        }
+
+        protected object ResolveUmbracoProperty()
+        {
+            var property = GetClosestProperty();
+
+            if (property == null)
+                return null;
+
+            var propertyValue = property.GetValue(); //.Value;
+
+            if (propertyValue is Udi[])
+            {
+                propertyValue = ((Udi[])propertyValue).Select(c => Current.UmbracoHelper.Content(c)).ToList();
+            }
+
+            //if the property value is an IPublishedContent array and needs mapping to a custom List
+            if (propertyValue is IEnumerable<IPublishedContent> && (Context.Property.PropertyType.GenericTypeArguments.Length > 0 && Context.Property.PropertyType.GenericTypeArguments[0] != typeof(IPublishedContent)))
+            {
+                var propertyValueEnumerable = (IEnumerable<IPublishedContent>)propertyValue;
+                var value = (IList)Activator.CreateInstance(Context.Property.PropertyType);
+                foreach (IPublishedContent item in propertyValueEnumerable)
+                {
+                    value.Add(item.As(Context.Property.PropertyType.GenericTypeArguments[0]));
+                }
+                return value;
+            }
+            //else if the property value is an array but the class property is a single item
+            else if (propertyValue is IEnumerable<IPublishedContent> && Context.Property.PropertyType == typeof(IPublishedContent))
+            {
+                return ((IEnumerable<IPublishedContent>)propertyValue).FirstOrDefault();
+            }
+            //if the property value is a HtmlString, convert to string
+            else if (propertyValue is HtmlString)
+            {
+                return propertyValue.ToString();
+            }
+
+            //otherwise return the untouched value
+            return propertyValue;
+        }
+    }
+}
