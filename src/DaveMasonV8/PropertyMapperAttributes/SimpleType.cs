7ebb4701359a23ed1539a384a9c225595124cdb2
Scott Rosseter <scott.rosseter@mentordigital.co.uk>
2020-02-25 13:02:58 +0000

    update for v8

--- src/DaveMasonV8/PropertyMapperAttributes/SimpleType.cs
+++ src/DaveMasonV8/PropertyMapperAttributes/SimpleType.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -43,21 +45,46 @@ namespace DaveMason.PropertyMapperAttributes
             if (property == null)
                 return null;
 
-            var propertyValue = property.GetValue(); //.Value;
+            var propertyValue = property.GetValue();
 
             if (propertyValue is Udi[])
             {
                 propertyValue = ((Udi[])propertyValue).Select(c => Current.UmbracoHelper.Content(c)).ToList();
             }
 
+            if (propertyValue is JObject && Context.Property.PropertyType != typeof(JObject))
+            {
+                if (Context.Property.PropertyType == typeof(string))
+                {
+                    propertyValue = propertyValue.ToString();
+                }
+                else if (Context.Property.PropertyType.IsClass)
+                {
+                    propertyValue = JsonConvert.DeserializeObject(propertyValue.ToString(), Context.Property.PropertyType);
+                }
+            }
+
             //if the property value is an IPublishedContent array and needs mapping to a custom List
             if (propertyValue is IEnumerable<IPublishedContent> && (Context.Property.PropertyType.GenericTypeArguments.Length > 0 && Context.Property.PropertyType.GenericTypeArguments[0] != typeof(IPublishedContent)))
             {
                 var propertyValueEnumerable = (IEnumerable<IPublishedContent>)propertyValue;
-                var value = (IList)Activator.CreateInstance(Context.Property.PropertyType);
+                //var value = (IList)Activator.CreateInstance(typeof(List<>).MakeGenericType(Context.Property.PropertyType));
+
+                var value = (IList)Activator.CreateInstance(typeof(List<>).MakeGenericType(Context.Property.PropertyType.GenericTypeArguments[0]));
+
+                var listType = typeof(List<>).MakeGenericType(Context.Property.PropertyType);
+                var list = (IList)Activator.CreateInstance(listType);
+
+
                 foreach (IPublishedContent item in propertyValueEnumerable)
                 {
-                    value.Add(item.As(Context.Property.PropertyType.GenericTypeArguments[0]));
+                    var arg = Context.Property.PropertyType.GenericTypeArguments[0];
+
+                    var typedItem = item.As(arg);
+
+                    value.Add(typedItem);
+
+                    //value.Add(item.As(Context.Property.PropertyType.GenericTypeArguments[0]));
                 }
                 return value;
             }
@@ -72,8 +99,26 @@ namespace DaveMason.PropertyMapperAttributes
                 return propertyValue.ToString();
             }
 
+            if (propertyValue != null)
+            {
+                if (propertyValue.GetType() != Context.Property.PropertyType && !propertyValue.GetType().Inherits(Context.Property.PropertyType))
+                {
+                    var type = propertyValue.GetType();
+                    if (type == typeof(GuidUdi))
+                    {
+                        Current.Logger.Warn(this.GetType(), "Trying to map unpublished content. Id: " + Context.Content.Id + ". Property: " + Context.Property.Name);
+                    }
+                    else
+                    {
+                        Current.Logger.Warn(this.GetType(), "Trying to map to wrong type. Id: " + Context.Content.Id + ". Property: " + Context.Property.Name + ". Wrong Type: " + Context.Property.PropertyType);
+                    }
+
+                    return Context.Property.PropertyType.GetType().GetDefaultValue();
+                }
+            }
             //otherwise return the untouched value
             return propertyValue;
         }
     }
 }
+
