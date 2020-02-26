22b71a71afd34fc5e75adad6f7c08c472715d297
Scott Rosseter <scott.rosseter@mentordigital.co.uk>
2020-02-26 13:29:05 +0000

    added V8 code generator and sandbox

---
+++ src/DaveMasonV8/Models/DaveMasonPropertyType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaveMasonV8.Models
+{
+    public class DaveMasonPropertyType
+    {
+        public string PropertyGroup { get; set; }
+        public string Name { get; set; }
+        public string PropertyType { get; set; }
+        public bool IsMandatory { get; set; }
+    }
+}
