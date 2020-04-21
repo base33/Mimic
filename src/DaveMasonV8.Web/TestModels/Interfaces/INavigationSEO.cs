7f001071c86cb2cdad3cdebf225483b7b8a5b7e9
Scott Rosseter <scott.rosseter@mentordigital.co.uk>
2020-04-21 15:18:41 +0100

    added some mapper tests

---
+++ src/DaveMasonV8.Web/TestModels/Interfaces/INavigationSEO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaveMasonV8.Web.TestModels.Interfaces
+{
+    public interface INavigationSEO
+    {
+        string SeoMetaDescription { get; set; }
+        bool UmbracoNavihide { get; set; }
+        IEnumerable<string> Keywords { get; set; }
+    }
+}
---
+++ src/DaveMasonV8.Web/TestModels/Models/Blogpost.cs
@@ -0,0 +1,24 @@
+﻿using DaveMasonV8.Web.TestModels.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DaveMasonV8.Web.TestModels.Models
+{
+    public class Blogpost : INavigationSEO
+    {
+        [Required]
+        public string Excerpt { get; set; }
+
+        [Required]
+        public string PageTitle { get; set; }
+
+        public IEnumerable<string> Categories { get; set; }
+        public string BodyText { get; set; }
+        public string SeoMetaDescription { get; set; }
+        public bool UmbracoNavihide { get; set; }
+        public IEnumerable<string> Keywords { get; set; }
+    }
+}
